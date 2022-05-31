09faa08289f61e791780cced9cd1526c1fece56a
Pelusa <agustinlorenzatto@hotmail.com>
2022-05-31 15:49:54 -0300

    Hice para que se actuailicen los precios y se remaque sin recargar la pagina, y los movimientos que no estan cargados quedan con el mismo precio

--- src/consultarCuenta/consultarCuenta.js
+++ src/consultarCuenta/consultarCuenta.js
@@ -1,5 +1,6 @@
 const {cerrarVentana,apretarEnter} = require('../helpers');
 const axios = require('axios');
+const { ipcRenderer } = require('electron/renderer');
 require("dotenv").config();
 const URL = process.env.URL;
 
@@ -8,6 +9,7 @@ const volver = document.querySelector('.volver');
 const tbodyVenta = document.querySelector(".listaVentas tbody");
 const tbodyProducto = document.querySelector(".listaProductos tbody");
 const actualizar = document.querySelector('.actualizar');
+const clienteInput = document.querySelector('#cliente');
 const saldo = document.querySelector('#saldo');
 
 let trSeleccionado = "";
@@ -18,6 +20,7 @@ buscar.addEventListener('keypress',async e=>{
         if (buscar.value !== "") {
             const cliente = (await axios.get(`${URL}clientes/id/${buscar.value}`)).data;
             saldo.value = (cliente.saldo).toFixed(2);
+            clienteInput.value = cliente.nombre;
             if (cliente === "") {
                 alert("Cliente no encontrado");
                 buscar.value = "";
@@ -26,11 +29,33 @@ buscar.addEventListener('keypress',async e=>{
                 const ventas = (await axios.get(`${URL}compensada/traerCompensadas/${cliente._id}`)).data;
                 listarVentas(ventas);
             }
+        }else{
+            const options = {
+                path: './clientes/clientes.html',
+                botones:false
+            }
+            ipcRenderer.send('abrir-ventana',options)
         }
     }
 });
 
+
+//Recibimos el cliente si lo buscamos por nombre
+ipcRenderer.on('recibir',async (e,args)=>{
+    const {tipo,informacion} = JSON.parse(args);
+    if (tipo === "cliente") {
+        const ventas = (await axios.get(`${URL}compensada/traerCompensadas/${informacion}`)).data;
+        const cliente = (await axios.get(`${URL}clientes/id/${informacion}`)).data;
+        saldo.value = cliente.saldo;
+        clienteInput.value = cliente.nombre;
+        
+        listarVentas(ventas)
+        
+    }
+})
+
 const listarVentas = async(lista)=>{
+    tbodyVenta.innerHTML = "";
     lista.forEach(venta=>{
         const date = new Date(venta.fecha);
         let day = date.getDate();
@@ -69,6 +94,7 @@ tbodyVenta.addEventListener('click',async e=>{
 });
 
 const listarProductos = async(movimientos)=>{
+    tbodyProducto.innerHTML = "";
     movimientos.forEach(movimiento=>{
         const date = new Date(movimiento.fecha);
         let day = date.getDate();
@@ -96,7 +122,6 @@ const listarProductos = async(movimientos)=>{
 //cuando tocamos actualizar una venta, actualizamos con los precios de hoy en dia
 actualizar.addEventListener('click',async e=>{
     if(trSeleccionado){
-
         const cuentaCompensada = (await axios.get(`${URL}compensada/traerCompensada/id/${trSeleccionado.id}`)).data;
         const cuentaHistorica = (await axios.get(`${URL}historica/PorId/id/${trSeleccionado.id}`)).data;
         const movimientos = (await axios.get(`${URL}movimiento/${trSeleccionado.id}`)).data;
@@ -105,10 +130,9 @@ actualizar.addEventListener('click',async e=>{
         let total = 0;
         for await(let movimiento of movimientos){
             const precio = (await axios.get(`${URL}productos/traerPrecio/${movimiento.codProd}`)).data;
-            movimiento.precio = precio;
+            movimiento.precio = precio !== "" ? precio : movimiento.precio;
             total += (precio*movimiento.cantidad);
         };
-
         if (confirm("Grabar Importe")) {
             total = parseFloat(total.toFixed(2));
             let cuentasHistoricasRestantes = (await axios.get(`${URL}historica/traerPorCliente/${cuentaHistorica.idCliente}`)).data;
@@ -132,6 +156,11 @@ actualizar.addEventListener('click',async e=>{
             await axios.put(`${URL}ventas/id/${venta._id}`,venta);
             await axios.put(`${URL}compensada/traerCompensada/id/${cuentaCompensada._id}`,cuentaCompensada);
             await axios.put(`${URL}historica/PorId/id/${cuentaHistorica._id}`,cuentaHistorica);
+            const cuentaModificada = (await axios.get(`${URL}compensada/traerCompensada/id/${trSeleccionado.id}`)).data;
+            listarProductos(movimientos)
+            trSeleccionado.children[4].innerHTML = cuentaModificada.importe;
+            trSeleccionado.children[6].innerHTML = cuentaModificada.saldo;
+            saldo.value = cliente.saldo;
         }
     }
 })
